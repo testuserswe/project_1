4e30985eecb20a477239e1dd9e37a8d176770675
testuserswe <testusertest2006@gmail.com>
2024-03-04 14:48:47 +0500

    adds

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from blog import views
 
 
 
 app_name = "blog"
 
 urlpatterns = [
+    path('', views.home, name="home"),
     
 ]
--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv()
 
-SECRET_KEY = os.environ.get('SECRET_KEY ')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 DEBUG = True
